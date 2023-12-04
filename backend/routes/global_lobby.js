784bc6de8348b57bb616a3db68b0d8ee6a7b2474
Louis Houston <louishouston98@gmail.com>
2023-12-03 19:35:20 -0800

    ready to

--- backend/routes/global_lobby.js
+++ backend/routes/global_lobby.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (_request, response) => {
-    response.render("global_lobby.ejs");
+    response.render("global_lobby.ejs", { pageTitle: "Lobby", pageContent: "Welcome" });
 });
+
 
 module.exports = router;
--- backend/routes/landing.js
+++ backend/routes/landing.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (_request, response) => {
-    response.render("landing.ejs");
+    response.render("landing.ejs", { pageTitle: "Home", pageContent: "Welcome" });
 });
 
 module.exports = router;
