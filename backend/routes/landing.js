f463e33e42106bcf2f56873036078d43a7234188
Nina Young <ninayoung@Ninas-MacBook-Air.local>
2023-12-10 21:21:46 -0800

    added logo to navbar

--- backend/routes/landing.js
+++ backend/routes/landing.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get("/", (_request, response) => {
     const user = _request.session.user;
     const loggedIn = _request.session.loggedIn;
-    response.render("landing.ejs", { pageTitle: "Home", pageContent: "Welcome", loggedIn: _request.session.loggedIn, user});
+    response.render("landing.ejs", { pageTitle: "Home", pageContent: "", loggedIn: _request.session.loggedIn, user});
 });
 
 module.exports = router;
